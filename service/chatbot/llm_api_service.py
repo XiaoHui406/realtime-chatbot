9e387dcea269426e9832e3e1d8c3d7ef6710c368
XiaoHui406 <1052120423@qq.com>
2026-05-20 00:26:51 +0800

    pref: 重构LLMApiService的chat方法

--- service/chatbot/llm_api_service.py
+++ service/chatbot/llm_api_service.py
@@ -11,7 +11,9 @@ from openai.types.chat import ChatCompletionMessageParam, \
     ChatCompletionUserMessageParam, \
     ChatCompletionAssistantMessageParam, \
     ChatCompletionToolMessageParam, \
-    ChatCompletionFunctionToolParam
+    ChatCompletionFunctionToolParam, \
+    ChatCompletionMessageFunctionToolCall
+from openai.types.chat.chat_completion_message_function_tool_call import Function
 
 from service.chatbot.interface.chatbot_service import ChatbotService
 
@@ -45,36 +47,23 @@ class LLMApiService(ChatbotService):
             )
 
         # call_no_reply初始为false，在大模型调用no_reply后变为true
-        self.call_no_reply: bool = False
+        # self.call_no_reply: bool = False
 
     async def chat(self, message: str) -> AsyncGenerator[str, None]:
-        assert self.llm_model
+        if not self.llm_model:
+            raise ValueError('llm model is not set')
         print(f'{self.messages=}')
 
+        user_message = ChatCompletionUserMessageParam(
+            role='user', content=message
+        )
+        self.messages.append(user_message)
+
         while True:
-            last_message_content = ''
-            # 如果不回复，则把这次message拼接到上次message中
-            if self.call_no_reply:
-                last_message = self.messages.pop()
-                print(f'{last_message=}')
-                assert type(last_message) is ChatCompletionUserMessageParam and type(
-                    last_message['content']) is str
-                last_message_content = last_message['content']
-                # 拼接完成后，把call_no_reply置为false，并继续运行
-                self.call_no_reply = False
-
-            # 将消息放入消息列表里
-            last_message_content += message
-            user_message = ChatCompletionUserMessageParam(
-                role='user', content=last_message_content
-            )
-            self.messages.append(user_message)
-
             # 发起请求并获得大模型的回复
             response_stream = await self.client.chat.completions.create(
                 model=self.llm_model,
                 messages=self.messages,
-                tools=[no_reply_json_schema],
                 stream=True,
                 extra_body={
                     "thinking": {
@@ -84,8 +73,12 @@ class LLMApiService(ChatbotService):
             )
 
             response_content_list: List[str] = []
+            tool_call_map: Dict[int,
+                                ChatCompletionMessageFunctionToolCall] = {}
+            tool_call_args_map: Dict[int, List[str]] = {}
             async for chunk in response_stream:
-                delta = chunk.choices[0].delta
+                choice = chunk.choices[0]
+                delta = choice.delta
                 # 大模型回复
                 if delta.content:
                     print(f'{delta.content=}')
@@ -94,33 +87,32 @@ class LLMApiService(ChatbotService):
 
                 elif delta.tool_calls:
                     for tool_call in delta.tool_calls:
-                        # 大模型调用no_reply方法
-                        # 将call_no_reply置为true并结束执行
-                        if tool_call.function and tool_call.function.name == 'no_reply':
-                            self.call_no_reply = True
-                            return
+                        if tool_call.function:
+                            if tool_call.id and tool_call.function.name:
+                                tool_call_map[tool_call.index] = ChatCompletionMessageFunctionToolCall(
+                                    id=tool_call.id,
+                                    type='function',
+                                    function=Function(
+                                        name=tool_call.function.name,
+                                        arguments=''
+                                    )
+                                )
+                            elif tool_call.function.arguments:
+                                tool_call_args_map.setdefault(tool_call.index, []).append(
+                                    tool_call.function.arguments
+                                )
+                elif not delta.tool_calls and choice.finish_reason == 'tool_calls':
+                    for (index, tool_call) in tool_call_map.items():
+                        tool_call.function.arguments = ''.join(
+                            tool_call_args_map[index])
+                        ...
 
-            # 回复列表长度大于0，说明有回复
-            # 把回复存入消息列表
-            if len(response_content_list) > 0:
-                response_message = ''.join(response_content_list)
-                print(f'{response_message=}')
-                self.messages.append(
-                    ChatCompletionAssistantMessageParam(
-                        role='assistant', content=response_message
-                    )
-                )
-                return
-
-
-no_reply_json_schema = ChatCompletionFunctionToolParam(
-    type='function',
-    function=FunctionDefinition(
-        name='no_reply',
-        description='如果你觉得用户没有说完，请调用该方法表示不回复',
-        parameters={
-            'type': 'object',
-            "properties": {},
-        }
-    )
-)
+                elif choice.finish_reason == 'stop':
+                    if response_content_list:
+                        response_content = ''.join(response_content_list)
+                        self.messages.append(ChatCompletionAssistantMessageParam(
+                            role="assistant", content=response_content
+                        ))
+                    return
+                else:
+                    continue
