6637d478a37485502ada79b8ed24972d156f7e14
XiaoHui406 <1052120423@qq.com>
2026-07-05 00:55:19 +0800

    fix: 修复 AppConfig getter 改为 const，支持默认参数; 添加 geolocator 依赖

--- app/lib/services/config.dart
+++ app/lib/services/config.dart
@@ -2,6 +2,6 @@ class AppConfig {
   static const String host = '127.0.0.1';
   static const int port = 8000;
 
-  static String get httpBaseUrl => 'http://$host:$port';
-  static String get wsBaseUrl => 'ws://$host:$port';
+  static const String httpBaseUrl = 'http://$host:$port';
+  static const String wsBaseUrl = 'ws://$host:$port';
 }
